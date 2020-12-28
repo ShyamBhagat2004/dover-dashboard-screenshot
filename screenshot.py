220b1de8dc4bfc41d0173f36fb41bc7e9b33c6d9
wearethedropperz <67416253+wearethedropperz@users.noreply.github.com>
2020-12-28 17:16:24 +0800

    Not  working  on  windows. Trying  on  mac

---
+++ screenshot.py
@@ -0,0 +1,21 @@
+from selenium import webdriver
+from PIL import Image
+
+# take screenshot
+driver = webdriver.Chrome();
+driver.get('https://www.google.com');
+element = driver.find_element_by_id("hplogo");
+location = element.location;
+size = element.size;
+driver.save_screenshot("pageImage.png");
+"""
+# crop image
+x = location['x'];
+y = location['y'];
+width = location['x']+size['width'];
+height = location['y']+size['height'];
+im = Image.open('pageImage.png')
+im = im.crop((int(x), int(y), int(width), int(height)))
+im.save('element.png')
+"""
+driver.quit()
